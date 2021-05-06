0af216e59026b509491dcdeddb3352e40e3a46ef
h3557984 <71916582+h3557984@users.noreply.github.com>
2021-05-06 23:09:50 +0800

    Create Main.R

---
+++ Main.R
@@ -0,0 +1,11 @@
+# Install and load the necessary libraries
+install.packages("fabricatr")
+install.packages("RMySQL")
+install.packages("magrittr")
+install.packages("ggplot2")
+
+library(fabricatr)
+library(RMySQL)
+library(magrittr)
+library(ggplot2)
+
